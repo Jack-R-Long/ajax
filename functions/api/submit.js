a641ca5edc33278c00257ff3b245e69f20e517e9
Jack Long <jack.richard.long@gmail.com>
2023-01-11 15:51:06 -0600

    try to hit API with env key

--- functions/api/submit.js
+++ functions/api/submit.js
@@ -2,10 +2,34 @@
 export async function onRequestPost(context) {
   try {
     let input = await context.request.formData();
-    let pretty = JSON.stringify([...input], null, 2);
-    return new Response(pretty, {
-      headers: { "Content-Type": "application/json;charset=utf-8" },
-    });
+
+    // Created with ChatGPT
+    const apiKey = context.env.OPENAI_API_KEY;
+    const model = "text-davinci-003";
+    const endpoint = "https://api.openai.com/v1/completions";
+
+    // Set up the request body
+    const body = {
+      model,
+      prompt: "Write a short story about a tricycle.",
+    };
+
+    // Set up the request options
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${apiKey}`,
+      },
+      body: JSON.stringify(body),
+    };
+
+    // Send the request to the OpenAI API
+    const response = await fetch(endpoint, options);
+
+    // Return the response from the OpenAI API as the response to the original request
+    return response;
+
   } catch (err) {
     return new Response("Error parsing JSON content", { status: 400 });
   }
